a2679872801c6783f210760f83a1677e12a6be5b
addffa <adli.daffa5@gmail.com>
2020-12-30 09:51:17 +0700

    update cassandra config to support 2 node

--- backend/settings.py
+++ backend/settings.py
@@ -1,11 +1,11 @@
 DATABASE = {
     'keyspace': {
         'name': 'hotelbook',
-        'replication_factor': 1,
+        'replication_factor': 2,
     },
     'config': {
         'default_keyspace': 'hotelbook',
-        'hosts': ['172.18.0.2', ],
+        'hosts': ['172.18.0.2', '172.18.0.3'],
         'retry_connect': True,
         'port': 9042
     }
