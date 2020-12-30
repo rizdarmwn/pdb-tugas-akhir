1706d623cd017ec77e35dcd9ecffc348262d9315
addffa <adli.daffa5@gmail.com>
2020-12-30 16:12:24 +0700

    update model and query

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import db
 from typing import Optional
 from fastapi import FastAPI, Request
 
-from models import Item, Room, RoomBase, Book, BookBase
+from models import Room, RoomBase, Book, BookBase
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
@@ -19,10 +19,6 @@ templates = Jinja2Templates(directory="templates")
 def read_root(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "hello": "Hello World"})
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
 
 # CRUD Room
 @app.get("/room")
@@ -69,14 +65,14 @@ def update_room(room_id: str, room: RoomBase):
 # CRUD Book
 @app.get("/book")
 def get_all_book():
-    books = Book.objects().all()
-    return {"status_code": 200, "data": list(books)}
+    books = list(Book.objects.all())
+    return {"status_code": 200, "data": books}
 
 
-@app.get("/book/{book_id}")
-def get_book(book_id: str):
-    book = Book.get(id=book_id)
-    return {"status_code": 200, "data": book}
+@app.get("/books/{guest_email}")
+def get_user_book(guest_email: str):
+    books = list(Book.filter(guest_email=guest_email))
+    return {"status_code": 200, "data": books}
 
 
 @app.post("/book")
@@ -85,28 +81,24 @@ def create_book(book: BookBase):
         guest_email=book.guest_email,
         room_id=book.room_id,
         number_of_guest=book.number_of_guest,
-        status="process",
+        status="Booked",
         checkin_date=book.checkin_date,
         checkout_date=book.checkout_date,
     )
     return {"status_code": 200, "data": new_book}
 
 
-@app.put("/book/{book_id}")
-def update_book(book_id: str, book: BookBase):
-    updated_book = Book.get(id=book_id).update(
-        guest_email=book.guest_email,
-        room_id=book.room_id,
-        number_of_guest=book.number_of_guest,
-        checkin_date=book.checkin_date,
-        checkout_date=book.checkout_date,
+@app.put("/book/{guest_email}/{book_id}/checkout")
+def checkout_user_book(guest_email: str, book_id: str):
+    updated_book = Book.get(guest_email=guest_email, id=book_id).update(
+        status="Check-Out"
     )
     return {"status_code": 200, "data": updated_book}
 
 
-@app.put("/book/{book_id}/status")
-def update_book_status(book_id: str, status: str):
-    updated_book = Book.get(id=book_id).update(
-        status=status
+@app.put("/book/{guest_email}/{book_id}/status")
+def cancel_user_book(guest_email: str, book_id: str):
+    updated_book = Book.get(guest_email=guest_email, id=book_id).update(
+        status="Canceled"
     )
     return {"status_code": 200, "data": updated_book}
